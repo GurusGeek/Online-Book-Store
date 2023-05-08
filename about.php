8c1565f154d223b70f90165871ac05cc888700ac
Aabideen <p200006@pwr.nu.edu.pk>
2023-05-08 15:52:23 +0500

    added

---
+++ about.php
@@ -0,0 +1,241 @@
+/* The above code is a PHP file that displays an "About Us" page for a bookstore website. It includes a
+header and footer file, and displays information about the bookstore, client reviews, and great
+authors. It also includes links to the home page and contact page. The code checks if the user is
+logged in and redirects them to the login page if they are not. It also includes CSS and JavaScript
+files for styling and functionality. */
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">Home</a> / About </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about-img.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Why Choose Us?</h3>
+         <p>At our bookstore, we take pride in providing our customers with a superior shopping experience. We understand that there are many options available when it comes to purchasing books online, which is why we offer several compelling reasons why you should choose us:
+
+</p>
+      
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">Client's Reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/pic-1.png" alt="">
+         <p>"I was blown away by the selection of books on this website. It was so easy to find exactly what I was looking for, and the pricing was very reasonable. I'll definitely be coming back!" - Sarah M.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         
+      </div>
+
+      <div class="box">
+         <img src="images/pic-2.png" alt="">
+         <p>"I had an issue with my order and reached out to customer service for help. They were incredibly responsive and resolved my issue quickly. I was very impressed with their customer service!" - Tom B.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+       
+      </div>
+
+      <div class="box">
+         <img src="images/pic-3.png" alt="">
+         <p>"I love the convenience of shopping for books online, and this website definitely delivers. The checkout process was a breeze, and I received my order in just a few days. Highly recommend!" - Emily W.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         
+      </div>
+
+      <div class="box">
+         <img src="images/pic-4.png" alt="">
+         <p>"As a busy student, I don't always have time to go to the bookstore. This website makes it so easy to purchase the books I need for class, and the prices are much more affordable than my university bookstore. Thank you!" - Jake K.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+   
+      </div>
+
+      <div class="box">
+         <img src="images/pic-5.png" alt="">
+         <p>"I've been searching for a specific book for months and was thrilled to find it on this website. The ordering process was seamless, and I received the book in perfect condition. I'll definitely be returning for my future book needs!" - Heather S.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+       
+      </div>
+
+      <div class="box">
+         <img src="images/pic-6.png" alt="">
+         <p>"I was hesitant to purchase books online, but this website exceeded my expectations. The customer service was excellent, the shipping was fast, and the books arrived in great condition. I'm a satisfied customer!" - John D.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+       
+      </div>
+
+   </div>
+
+</section>
+
+<section class="authors">
+
+   <h1 class="title">Our Authors</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/author-1.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Ali</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-2.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Sarah</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-3.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Jimmy</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-4.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Maryam Nawaz</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-5.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Elon Musk</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-6.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Abida Parveen</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="PAGES/contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Info</h3>
+         <p> <i class="fas fa-phone"></i> +92309-9655-934 </p>
+         <p> <i class="fas fa-phone"></i> +92336-8673-121 </p>
+         <p> <i class="fas fa-envelope"></i> Aabideenbofficial@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Peshawar, Pakistan - 25000 </p>
+      </div>
+
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; Copyright  @ <?php echo date('Y'); ?> by <span>Aabideen</span> </p>
+
+</section>
